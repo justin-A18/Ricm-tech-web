8120c0769ecf165cb6c56d63428c74ce45a71100
Justin Huertas <justin.hv08@gmail.com>
2025-05-26 00:07:54 -0500

    :sparkles: feat: add first version of the presentation and services section

---
+++ src/constants/logos.ts
@@ -0,0 +1,39 @@
+import Typescript from '../assets/typescript.svg';
+import Astro from '../assets/astro.svg';
+import Java from '../assets/java.svg';
+import Linux from '../assets/linux.svg';
+import React from '../assets/react.svg';
+import WordPress from '../assets/wordpress.svg';
+
+export const LOGOS = [
+	{
+		position: 1,
+		name: 'TypeScript',
+		logo: Typescript,
+	},
+	{
+		position: 2,
+		name: 'Astro',
+		logo: Astro,
+	},
+	{
+		position: 3,
+		name: 'Java',
+		logo: Java,
+	},
+	{
+		position: 4,
+		name: 'Linux',
+		logo: Linux,
+	},
+	{
+		position: 5,
+		name: 'React',
+		logo: React,
+	},
+	{
+		position: 6,
+		name: 'WordPress',
+		logo: WordPress,
+	},
+];
