1a09a707533b9ebaa7b517a4596cab53d0b01682
Justin Huertas <justin.hv08@gmail.com>
2025-09-15 17:37:39 -0500

    :art: chore(ui): reorganize footer, add mobile dropdown menu and new contact icons

---
+++ src/constants/navigation.ts
@@ -0,0 +1,14 @@
+export const NAV_LINKS = [
+	{
+		name: 'Inicio',
+		href: '#inicio'
+	},
+	{
+		name: 'Servicios',
+		href: '#servicios'
+	},
+	{
+		name: 'Proyectos',
+		href: '#proyectos'
+	}
+];
